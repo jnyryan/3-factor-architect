d65b3f4f35979dd22a9b1a64d08755a4f863b388
John Ryan <jnyryan@gmail.com>
2014-06-18 16:32:36 +0100

    set relative paths

--- web/server.js
+++ web/server.js
@@ -8,10 +8,8 @@ var app = express();
 app.use(logfmt.requestLogger());
 app.use(express.static(__dirname + '/public/'));
 
-console.log(__dirname + '/public/');
-
 app.get('/', function(req, res) {
-    res.sendfile('./views/index.html');
+    res.sendfile('./web/views/index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
